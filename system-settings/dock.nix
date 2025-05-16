7d29a6ecbbe68622e8fe991110b589b8f88b64a1
Niklas Scholz <niklas.scholz@outlook.com>
2025-05-17 08:50:54 +1000

    dock

---
+++ system-settings/dock.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+{
+  system.defaults = {
+    dock = {
+      # Automatically hide and show the Dock
+      autohide = true;
+      # Whether to group windows by application in Mission Control’s Exposé. The default is false.
+      expose-group-apps = true;
+      # Set the icon size of Dock items to 24 pixels
+      tilesize = 24;
+      # show indicator lights for open applications in the dock
+      show-process-indicators = true;
+      # Don’t animate opening applications from the Dock
+      launchanim = false;
+      # Speed up Mission Control animations
+      expose-animation-duration = 0.1;
+      # Remove the auto-hiding Dock delay
+      autohide-delay = 0.0;
+      # Remove the animation when hiding/showing the Dock
+      autohide-time-modifier = 0;
+      # Make Dock icons of hidden applications translucent
+      showhidden = true;
+      # Don’t show recent applications in Dock
+      show-recents = false;
+      # Show appswitcher on all windows
+      appswitcher-all-displays = true;
+
+      # Disable hot corners
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+    };
+  };
+}
--- system-settings/os-general.nix
+++ system-settings/os-general.nix
@@ -3,11 +3,5 @@
   system.defaults = {
     # For some reason, mission control doesn’t like that AeroSpace puts a lot of windows in the bottom right corner of the screen. Mission control shows windows too small even there is enough space to show them bigger.
     NSGlobalDomain.NSWindowShouldDragOnGesture = true;
-    dock = {
-      autohide = true;
-      # Whether to group windows by application in Mission Control’s Exposé. The default is false.
-      expose-group-apps = true;
-
-    };
   };
 }
