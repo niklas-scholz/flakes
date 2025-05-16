bae6e647c3fed38aee7998e756aab855521ce2f5
Niklas Scholz <niklas.scholz@outlook.com>
2025-05-17 09:59:09 +1000

    keyboard

---
+++ system-settings/keyboard.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  system.defaults.NSGlobalDomain = {
+    # Set a blazingly fast keyboard repeat rate
+    KeyRepeat = 1;
+    InitialKeyRepeat = 10;
+    # Disable press-and-hold for keys in favor of key repeat
+    ApplePressAndHoldEnabled = false;
+
+    # Enable full keyboard access for all controls
+    # (e.g. enable Tab in modal dialogs)
+    AppleKeyboardUIMode = 3;
+  };
+
+}
--- system-settings/peripherals.nix
+++ system-settings/peripherals.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./trackpad.nix
+    ./keyboard.nix
   ];
 
 }
