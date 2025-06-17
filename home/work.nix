23e3666279e725b7ce77100789cf05ce93a7527e
Niklas Scholz <niklas.scholz@propelleraero.com.au>
2025-06-18 07:23:44 +1000

    some things

--- home/work.nix
+++ home/work.nix
@@ -2,9 +2,15 @@
 {
   programs = {
     zsh = {
+      shellAliases = {
+        pyac = "source $(poetry env info --path)/bin/activate";
+      };
       sessionVariables = {
         FOO = "bar";
       };
+    };
+    pyenv = {
+      enableZshIntegration = true;
     };
   };
 }
