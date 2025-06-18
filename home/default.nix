cf521c2f3b39743d7085b65501274546ed8cf108
Niklas Scholz <niklas.scholz@outlook.com>
2025-06-18 21:53:49 +1000

    cleanup

--- home/default.nix
+++
@@ -1,9 +0,0 @@
-{ username, ... }:
-{
-  home.username = username;
-  home.stateVersion = "25.05";
-  home.enableNixpkgsReleaseCheck = false;
-  programs = import ./shell.nix // {
-    # ...
-  };
-}
