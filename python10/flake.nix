8f0e743cc6361bfbedabdb67c2417c2c1b74bf7f
Niklas Scholz <niklas.scholz@propelleraero.com.au>
2025-10-28 14:15:52 +1100

    better utils

--- python10/flake.nix
+++ python10/flake.nix
@@ -1,54 +1,55 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      # Create a function to generate a devShell for each supported system.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
     {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.python310
-              pkgs.pipx
-            ];
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
 
-            shellHook = ''
-              # 1. Create temporary directories inside a Nix-managed temporary space
-              export PIPX_HOME="$(mktemp -d)"
-              export PIPX_BIN_DIR="$PIPX_HOME/bin"
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-              # 2. Add the new bin directory to the shell's PATH
-              export PATH="$PIPX_BIN_DIR:$PATH"
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.python310
+            pkgs.pipx
+          ];
 
-              # 3. Ensure pipx uses the new paths 
-              ${pkgs.pipx}/bin/pipx ensurepath --force
+          shellHook = ''
+            # 1. Create temporary directories inside a Nix-managed temporary space
+            export PIPX_HOME="$(mktemp -d)"
+            export PIPX_BIN_DIR="$PIPX_HOME/bin"
 
-              # 4. Install poetry
-              echo "Installing poetry (1.5.1) into Nix-managed temporary environment..."
-              pipx install poetry==1.5.1 
+            # 2. Add the new bin directory to the shell's PATH
+            export PATH="$PIPX_BIN_DIR:$PATH"
 
-              echo "Poetry is now available at: $(which poetry)"
-            '';
-          };
-        }
-      );
-    };
+            # 3. Ensure pipx uses the new paths 
+            ${pkgs.pipx}/bin/pipx ensurepath --force
+
+            # 4. Install poetry
+            echo "Installing poetry (1.5.1) into Nix-managed temporary environment..."
+            pipx install poetry==1.5.1 
+
+            echo "Poetry is now available at: $(which poetry)"
+          '';
+        };
+      in
+      {
+        devShells = {
+          default = devShell;
+        };
+      }
+    );
 }
