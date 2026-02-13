d43e987a09eea1653d2a27a99df73eae756ab49e
Niklas Scholz <niklas.scholz@propelleraero.com.au>
2026-02-13 17:47:57 +1100

    corepack

--- darwin/home/mkHomeConfiguration.nix
+++ darwin/home/mkHomeConfiguration.nix
@@ -6,12 +6,23 @@
 let
   pkgs = import <nixpkgs> { };
   standardConfig = {
-    home.username = username;
-    home.stateVersion = "25.05";
-    home.enableNixpkgsReleaseCheck = false;
-    home.sessionPath = [
-      "$(yarn global bin):$PATH"
-    ];
+    home = {
+      inherit username;
+      stateVersion = "25.05";
+      enableNixpkgsReleaseCheck = false;
+      sessionPath = [
+        "$(corepack yarn global bin):$PATH"
+      ];
+      packages = with pkgs; [
+        nodejs
+        (writeShellScriptBin "pnpm" ''
+          exec ${nodejs_20}/bin/corepack pnpm "$@"
+        '')
+        (writeShellScriptBin "yarn" ''
+          exec ${nodejs_20}/bin/corepack yarn "$@"
+        '')
+      ];
+    };
   };
   shellConfig = import ./shell.nix;
 in
@@ -19,18 +30,18 @@ in
   imports = [
     home-manager.darwinModules.home-manager
     {
-
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        backupFileExtension = "backup";
+        users.${username} = pkgs.lib.mkMerge [
+          standardConfig
+          shellConfig
+          extraConfig
+        ];
+      };
 
       users.users.${username}.home = "/Users/${username}";
-      home-manager.backupFileExtension = "backup";
-
-      home-manager.users.${username} = pkgs.lib.mkMerge [
-        standardConfig
-        shellConfig
-        extraConfig
-      ];
     }
   ];
 }
